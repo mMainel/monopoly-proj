4fc9fe59e5c2d0191de8c3001b00fb70f4f679bd
Emanuel Teixeira <emanuel.valladao@econodata.com.br>
2025-11-11 14:21:00 -0300

    Fix: curso da posicao

--- modules/cartasJogo.py
+++ modules/cartasJogo.py
@@ -43,12 +43,12 @@ def criar_cartas_sorte(jogo) -> list:
     )
     cartas.append(carta3)
 
-    def ir_posicao_24(jogador):
-        jogador.irPara(24)
+    def ir_posicao_21(jogador):
+        jogador.irPara(21)
 
     carta4 = CartaSorte(
-        "Você foi convidado para uma palestra na Economia. Avance até lá",
-        ir_posicao_24
+        "Você foi convidado para uma palestra em Ciências Econômicas. Avance até lá",
+        ir_posicao_21
     )
     cartas.append(carta4)
 
